2545c3e8e8517933e10137f30c9cd37bd1e8650c
Niranjan_Bala <niranjan.veerasekaran@commonfloor.com>
2015-04-16 21:25:35 +0530

    photos

---
+++ public/imageprops.js
@@ -0,0 +1,657 @@
+var imagesprop=[{
+    "Image": 3938,
+    "Category": "When the Lights go down",
+    "Latitude": "12.916529 North",
+    "Longitude": "77.592567 East",
+    "IMAGE": "IMG_3938.jpg",
+    "lat": 12.916529,
+    "lng": 77.592567
+}, {
+    "Image": 3942,
+    "Category": "When the Lights go down",
+    "Latitude": "12.916517 North",
+    "Longitude": "77.592513 East",
+    "IMAGE": "IMG_3942.jpg",
+    "lat": 12.916517,
+    "lng": 77.592513
+}, {
+    "Image": 3419,
+    "Category": "",
+    "Latitude": "12.917138 North",
+    "Longitude": "77.593385 East",
+    "IMAGE": "IMG_3419.jpg",
+    "lat": 12.917138,
+    "lng": 77.593385
+}, {
+    "Image": 3449,
+    "Category": "",
+    "Latitude": "12.918641 North",
+    "Longitude": "77.592358 East",
+    "IMAGE": "IMG_3449.jpg",
+    "lat": 12.918641,
+    "lng": 77.592358
+}, {
+    "Image": 3438,
+    "Category": "",
+    "Latitude": "12.919091 North",
+    "Longitude": "77.591805 East",
+    "IMAGE": "IMG_3438.jpg",
+    "lat": 12.919091,
+    "lng": 77.591805
+}, {
+    "Image": 3440,
+    "Category": "",
+    "Latitude": "12.92097 North",
+    "Longitude": "77.591089 East",
+    "IMAGE": "IMG_3440.jpg",
+    "lat": 12.92097,
+    "lng": 77.591089
+}, {
+    "Image": 3445,
+    "Category": "",
+    "Latitude": "12.92007 North",
+    "Longitude": "77.591013 East",
+    "IMAGE": "IMG_3445.jpg",
+    "lat": 12.92007,
+    "lng": 77.591013
+}, {
+    "Image": 3712,
+    "Category": "",
+    "Latitude": "12.926027 North",
+    "Longitude": "77.593517 East",
+    "IMAGE": "IMG_3712.jpg",
+    "lat": 12.926027,
+    "lng": 77.593517
+}, {
+    "Image": 3705,
+    "Category": "Landmarks",
+    "Latitude": "12.926475 North",
+    "Longitude": "77.590053 East",
+    "IMAGE": "IMG_3705.jpg",
+    "lat": 12.926475,
+    "lng": 77.590053
+}, {
+    "Image": 3526,
+    "Category": "",
+    "Latitude": "12.926514 North",
+    "Longitude": "77.585657 East",
+    "IMAGE": "IMG_3526.jpg",
+    "lat": 12.926514,
+    "lng": 77.585657
+}, {
+    "Image": 3527,
+    "Category": "",
+    "Latitude": "12.926574 North",
+    "Longitude": "77.585657 East",
+    "IMAGE": "IMG_3527.jpg",
+    "lat": 12.926574,
+    "lng": 77.585657
+}, {
+    "Image": 3528,
+    "Category": "",
+    "Latitude": "12.926574 North",
+    "Longitude": "77.585657 East",
+    "IMAGE": "IMG_3528.jpg",
+    "lat": 12.926574,
+    "lng": 77.585657
+}, {
+    "Image": 3544,
+    "Category": "Who's Next door",
+    "Latitude": "12.926813 North",
+    "Longitude": "77.585852 East",
+    "IMAGE": "IMG_3544.jpg",
+    "lat": 12.926813,
+    "lng": 77.585852
+}, {
+    "Image": 3957,
+    "Category": "When the Lights go down",
+    "Latitude": "12.927088 North",
+    "Longitude": "77.584374 East",
+    "IMAGE": "IMG_3957.jpg",
+    "lat": 12.927088,
+    "lng": 77.584374
+}, {
+    "Image": 3959,
+    "Category": "When the Lights go down",
+    "Latitude": "12.927068 North",
+    "Longitude": "77.584374 East",
+    "IMAGE": "IMG_3959.jpg",
+    "lat": 12.927068,
+    "lng": 77.584374
+}, {
+    "Image": 3960,
+    "Category": "When the Lights go down",
+    "Latitude": "12.927188 North",
+    "Longitude": "77.584374 East",
+    "IMAGE": "IMG_3960.jpg",
+    "lat": 12.927188,
+    "lng": 77.584374
+}, {
+    "Image": 3965,
+    "Category": "When the Lights go down",
+    "Latitude": "12.927018 North",
+    "Longitude": "77.584374 East",
+    "IMAGE": "IMG_3965.jpg",
+    "lat": 12.927018,
+    "lng": 77.584374
+}, {
+    "Image": 3678,
+    "Category": "",
+    "Latitude": "12.927093 North",
+    "Longitude": "77.584376 East",
+    "IMAGE": "IMG_3678.jpg",
+    "lat": 12.927093,
+    "lng": 77.584376
+}, {
+    "Image": 3686,
+    "Category": "Really good for",
+    "Latitude": "12.927193 North",
+    "Longitude": "77.584376 East",
+    "IMAGE": "IMG_3686.jpg",
+    "lat": 12.927193,
+    "lng": 77.584376
+}, {
+    "Image": 3690,
+    "Category": "Landmarks",
+    "Latitude": "12.927293 North",
+    "Longitude": "77.584376 East",
+    "IMAGE": "IMG_3690.jpg",
+    "lat": 12.927293,
+    "lng": 77.584376
+}, {
+    "Image": 3692,
+    "Category": "Lifestyle",
+    "Latitude": "12.927328 North",
+    "Longitude": "77.584054 East",
+    "IMAGE": "IMG_3692.jpg",
+    "lat": 12.927328,
+    "lng": 77.584054
+}, {
+    "Image": 3546,
+    "Category": "",
+    "Latitude": "12.927682 North",
+    "Longitude": "77.586559 East",
+    "IMAGE": "IMG_3546.jpg",
+    "lat": 12.927682,
+    "lng": 77.586559
+}, {
+    "Image": 3547,
+    "Category": "Lifestyle",
+    "Latitude": "12.927942 North",
+    "Longitude": "77.586771 East",
+    "IMAGE": "IMG_3547.jpg",
+    "lat": 12.927942,
+    "lng": 77.586771
+}, {
+    "Image": 3715,
+    "Category": "Really good for",
+    "Latitude": "12.927973 North",
+    "Longitude": "77.591165 East",
+    "IMAGE": "IMG_3715.jpg",
+    "lat": 12.927973,
+    "lng": 77.591165
+}, {
+    "Image": 3696,
+    "Category": "",
+    "Latitude": "12.928168 North",
+    "Longitude": "77.582902 East",
+    "IMAGE": "IMG_3696.jpg",
+    "lat": 12.928168,
+    "lng": 77.582902
+}, {
+    "Image": 3720,
+    "Category": "Homes and apartments",
+    "Latitude": "12.928267 North",
+    "Longitude": "77.592275 East",
+    "IMAGE": "IMG_3720.jpg",
+    "lat": 12.928267,
+    "lng": 77.592275
+}, {
+    "Image": 3548,
+    "Category": "Who's Next door",
+    "Latitude": "12.928356 North",
+    "Longitude": "77.587113 East",
+    "IMAGE": "IMG_3548.jpg",
+    "lat": 12.928356,
+    "lng": 77.587113
+}, {
+    "Image": 3550,
+    "Category": "Lifestyle",
+    "Latitude": "12.928378 North",
+    "Longitude": "77.587113 East",
+    "IMAGE": "IMG_3550.jpg",
+    "lat": 12.928378,
+    "lng": 77.587113
+}, {
+    "Image": 3552,
+    "Category": "Really good for",
+    "Latitude": "12.928356 North",
+    "Longitude": "77.587113 East",
+    "IMAGE": "IMG_3552.jpg",
+    "lat": 12.928356,
+    "lng": 77.587113
+}, {
+    "Image": 3522,
+    "Category": "",
+    "Latitude": "12.928739 North",
+    "Longitude": "77.582744 East",
+    "IMAGE": "IMG_3522.jpg",
+    "lat": 12.928739,
+    "lng": 77.582744
+}, {
+    "Image": 3949,
+    "Category": "When the Lights go down",
+    "Latitude": "12.929338 North",
+    "Longitude": "77.583071 East",
+    "IMAGE": "IMG_3949.jpg",
+    "lat": 12.929338,
+    "lng": 77.583071
+}, {
+    "Image": 3523,
+    "Category": "Lifestyle",
+    "Latitude": "12.929481 North",
+    "Longitude": "77.58317 East",
+    "IMAGE": "IMG_3523.jpg",
+    "lat": 12.929481,
+    "lng": 77.58317
+}, {
+    "Image": 3948,
+    "Category": "When the Lights go down",
+    "Latitude": "12.929775 North",
+    "Longitude": "77.583398 East",
+    "IMAGE": "IMG_3948.jpg",
+    "lat": 12.929775,
+    "lng": 77.583398
+}, {
+    "Image": 3497,
+    "Category": "Who's Next door",
+    "Latitude": "12.929815 North",
+    "Longitude": "77.583657 East",
+    "IMAGE": "IMG_3497.jpg",
+    "lat": 12.929815,
+    "lng": 77.583657
+}, {
+    "Image": 3500,
+    "Category": "",
+    "Latitude": "12.929812 North",
+    "Longitude": "77.583657 East",
+    "IMAGE": "IMG_3500.jpg",
+    "lat": 12.929812,
+    "lng": 77.583657
+}, {
+    "Image": 3521,
+    "Category": "",
+    "Latitude": "12.93096 North",
+    "Longitude": "77.584077 East",
+    "IMAGE": "IMG_3521.jpg",
+    "lat": 12.93096,
+    "lng": 77.584077
+}, {
+    "Image": 3495,
+    "Category": "",
+    "Latitude": "12.931121 North",
+    "Longitude": "77.584107 East",
+    "IMAGE": "IMG_3495.jpg",
+    "lat": 12.931121,
+    "lng": 77.584107
+}, {
+    "Image": 3519,
+    "Category": "",
+    "Latitude": "12.931336 North",
+    "Longitude": "77.584278 East",
+    "IMAGE": "IMG_3519.jpg",
+    "lat": 12.931336,
+    "lng": 77.584278
+}, {
+    "Image": 3504,
+    "Category": "",
+    "Latitude": "12.93149 North",
+    "Longitude": "77.583903 East",
+    "IMAGE": "IMG_3504.jpg",
+    "lat": 12.93149,
+    "lng": 77.583903
+}, {
+    "Image": 3494,
+    "Category": "Who's Next door",
+    "Latitude": "12.931627 North",
+    "Longitude": "77.584279 East",
+    "IMAGE": "IMG_3494.jpg",
+    "lat": 12.931627,
+    "lng": 77.584279
+}, {
+    "Image": 3517,
+    "Category": "",
+    "Latitude": "12.931794 North",
+    "Longitude": "77.584522 East",
+    "IMAGE": "IMG_3517.jpg",
+    "lat": 12.931794,
+    "lng": 77.584522
+}, {
+    "Image": 3516,
+    "Category": "Who's Next door",
+    "Latitude": "12.93221 North",
+    "Longitude": "77.584745 East",
+    "IMAGE": "IMG_3516.jpg",
+    "lat": 12.93221,
+    "lng": 77.584745
+}, {
+    "Image": 3514,
+    "Category": "",
+    "Latitude": "12.932492 North",
+    "Longitude": "77.584874 East",
+    "IMAGE": "IMG_3514.jpg",
+    "lat": 12.932492,
+    "lng": 77.584874
+}, {
+    "Image": 3515,
+    "Category": "",
+    "Latitude": "12.932452 North",
+    "Longitude": "77.584874 East",
+    "IMAGE": "IMG_3515.jpg",
+    "lat": 12.932452,
+    "lng": 77.584874
+}, {
+    "Image": 3503,
+    "Category": "",
+    "Latitude": "12.932694 North",
+    "Longitude": "77.584077 East",
+    "IMAGE": "IMG_3503.jpg",
+    "lat": 12.932694,
+    "lng": 77.584077
+}, {
+    "Image": 3559,
+    "Category": "",
+    "Latitude": "12.9331 North",
+    "Longitude": "77.585225 East",
+    "IMAGE": "IMG_3559.jpg",
+    "lat": 12.9331,
+    "lng": 77.585225
+}, {
+    "Image": 3555,
+    "Category": "",
+    "Latitude": "12.933465 North",
+    "Longitude": "77.585422 East",
+    "IMAGE": "IMG_3555.jpg",
+    "lat": 12.933465,
+    "lng": 77.585422
+}, {
+    "Image": 3556,
+    "Category": "Lifestyle",
+    "Latitude": "12.933365 North",
+    "Longitude": "77.585422 East",
+    "IMAGE": "IMG_3556.jpg",
+    "lat": 12.933365,
+    "lng": 77.585422
+}, {
+    "Image": 3659,
+    "Category": "Homes and apartments",
+    "Latitude": "12.933732 North",
+    "Longitude": "77.585056 East",
+    "IMAGE": "IMG_3659.jpg",
+    "lat": 12.933732,
+    "lng": 77.585056
+}, {
+    "Image": 3662,
+    "Category": "Homes and apartments",
+    "Latitude": "12.933736 North",
+    "Longitude": "77.585049 East",
+    "IMAGE": "IMG_3662.jpg",
+    "lat": 12.933736,
+    "lng": 77.585049
+}, {
+    "Image": 3649,
+    "Category": "Who's Next door",
+    "Latitude": "12.933756 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3649.jpg",
+    "lat": 12.933756,
+    "lng": 77.585012
+}, {
+    "Image": 3653,
+    "Category": "",
+    "Latitude": "12.933716 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3653.jpg",
+    "lat": 12.933716,
+    "lng": 77.585012
+}, {
+    "Image": 3657,
+    "Category": "Lifestyle",
+    "Latitude": "12.933726 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3657.jpg",
+    "lat": 12.933726,
+    "lng": 77.585012
+}, {
+    "Image": 3665,
+    "Category": "Homes and apartments",
+    "Latitude": "12.933736 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3665.jpg",
+    "lat": 12.933736,
+    "lng": 77.585012
+}, {
+    "Image": 3731,
+    "Category": "Homes and apartments",
+    "Latitude": "12.933746 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3731.jpg",
+    "lat": 12.933746,
+    "lng": 77.585012
+}, {
+    "Image": 3735,
+    "Category": "Homes and apartments",
+    "Latitude": "12.933756 North",
+    "Longitude": "77.585012 East",
+    "IMAGE": "IMG_3735.jpg",
+    "lat": 12.933756,
+    "lng": 77.585012
+}, {
+    "Image": 3485,
+    "Category": "Really good for",
+    "Latitude": "12.933771 North",
+    "Longitude": "77.585011 East",
+    "IMAGE": "IMG_3485.jpg",
+    "lat": 12.933771,
+    "lng": 77.585011
+}, {
+    "Image": 3487,
+    "Category": "",
+    "Latitude": "12.933791 North",
+    "Longitude": "77.585011 East",
+    "IMAGE": "IMG_3487.jpg",
+    "lat": 12.933791,
+    "lng": 77.585011
+}, {
+    "Image": 3488,
+    "Category": "Really good for",
+    "Latitude": "12.933741 North",
+    "Longitude": "77.585011 East",
+    "IMAGE": "IMG_3488.jpg",
+    "lat": 12.933741,
+    "lng": 77.585011
+}, {
+    "Image": 3490,
+    "Category": "",
+    "Latitude": "12.933801 North",
+    "Longitude": "77.585011 East",
+    "IMAGE": "IMG_3490.jpg",
+    "lat": 12.933801,
+    "lng": 77.585011
+}, {
+    "Image": 3477,
+    "Category": "",
+    "Latitude": "12.93394 North",
+    "Longitude": "77.585428 East",
+    "IMAGE": "IMG_3477.jpg",
+    "lat": 12.93394,
+    "lng": 77.585428
+}, {
+    "Image": 3564,
+    "Category": "",
+    "Latitude": "12.934095 North",
+    "Longitude": "77.585803 East",
+    "IMAGE": "IMG_3564.jpg",
+    "lat": 12.934095,
+    "lng": 77.585803
+}, {
+    "Image": 3567,
+    "Category": "Lifestyle",
+    "Latitude": "12.934092 North",
+    "Longitude": "77.585803 East",
+    "IMAGE": "IMG_3567.jpg",
+    "lat": 12.934092,
+    "lng": 77.585803
+}, {
+    "Image": 3571,
+    "Category": "Who's Next door",
+    "Latitude": "12.934082 North",
+    "Longitude": "77.585803 East",
+    "IMAGE": "IMG_3571.jpg",
+    "lat": 12.934082,
+    "lng": 77.585803
+}, {
+    "Image": 3641,
+    "Category": "",
+    "Latitude": "12.934097 North",
+    "Longitude": "77.585077 East",
+    "IMAGE": "IMG_3641.jpg",
+    "lat": 12.934097,
+    "lng": 77.585077
+}, {
+    "Image": 3644,
+    "Category": "Cover",
+    "Latitude": "12.934698 North",
+    "Longitude": "77.585191 East",
+    "IMAGE": "IMG_3644.jpg",
+    "lat": 12.934698,
+    "lng": 77.585191
+}, {
+    "Image": 3745,
+    "Category": "Lifestyle",
+    "Latitude": "12.937487 North",
+    "Longitude": "77.586917 East",
+    "IMAGE": "IMG_3745.jpg",
+    "lat": 12.937487,
+    "lng": 77.586917
+}, {
+    "Image": 3624,
+    "Category": "",
+    "Latitude": "12.940052 North",
+    "Longitude": "77.58567 East",
+    "IMAGE": "IMG_3624.jpg",
+    "lat": 12.940052,
+    "lng": 77.58567
+}, {
+    "Image": 3634,
+    "Category": "",
+    "Latitude": "12.940186 North",
+    "Longitude": "77.585645 East",
+    "IMAGE": "IMG_3634.jpg",
+    "lat": 12.940186,
+    "lng": 77.585645
+}, {
+    "Image": 3633,
+    "Category": "",
+    "Latitude": "12.940191 North",
+    "Longitude": "77.585654 East",
+    "IMAGE": "IMG_3633.jpg",
+    "lat": 12.940191,
+    "lng": 77.585654
+}, {
+    "Image": 3636,
+    "Category": "",
+    "Latitude": "12.940674 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3636.jpg",
+    "lat": 12.940674,
+    "lng": 77.585553
+}, {
+    "Image": 3639,
+    "Category": "",
+    "Latitude": "12.940811 North",
+    "Longitude": "77.585526 East",
+    "IMAGE": "IMG_3639.jpg",
+    "lat": 12.940811,
+    "lng": 77.585526
+}, {
+    "Image": 3631,
+    "Category": "Who's Next door",
+    "Latitude": "12.941109 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3631.jpg",
+    "lat": 12.941109,
+    "lng": 77.585553
+}, {
+    "Image": 3638,
+    "Category": "",
+    "Latitude": "12.941218 North",
+    "Longitude": "77.58545 East",
+    "IMAGE": "IMG_3638.jpg",
+    "lat": 12.941218,
+    "lng": 77.58545
+}, {
+    "Image": 3579,
+    "Category": "Who's Next door",
+    "Latitude": "12.942591 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3579.jpg",
+    "lat": 12.942591,
+    "lng": 77.585553
+}, {
+    "Image": 3585,
+    "Category": "Landmarks",
+    "Latitude": "12.942591 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3585.jpg",
+    "lat": 12.942591,
+    "lng": 77.585553
+}, {
+    "Image": 3597,
+    "Category": "",
+    "Latitude": "12.942581 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3597.jpg",
+    "lat": 12.942581,
+    "lng": 77.585553
+}, {
+    "Image": 3616,
+    "Category": "",
+    "Latitude": "12.942571 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3616.jpg",
+    "lat": 12.942571,
+    "lng": 77.585553
+}, {
+    "Image": 3621,
+    "Category": "Homes and apartments",
+    "Latitude": "12.942561 North",
+    "Longitude": "77.585553 East",
+    "IMAGE": "IMG_3621.jpg",
+    "lat": 12.942561,
+    "lng": 77.585553
+}, {
+    "Image": 3608,
+    "Category": "Who's Next door",
+    "Latitude": "12.943629 North",
+    "Longitude": "77.582216 East",
+    "IMAGE": "IMG_3608.jpg",
+    "lat": 12.943629,
+    "lng": 77.582216
+}, {
+    "Image": 3599,
+    "Category": "",
+    "Latitude": "12.943699 North",
+    "Longitude": "77.580927 East",
+    "IMAGE": "IMG_3599.jpg",
+    "lat": 12.943699,
+    "lng": 77.580927
+}, {
+    "Image": 3601,
+    "Category": "Homes and apartments",
+    "Latitude": "12.943699 North",
+    "Longitude": "77.580937 East",
+    "IMAGE": "IMG_3601.jpg",
+    "lat": 12.943699,
+    "lng": 77.580937
+}];
